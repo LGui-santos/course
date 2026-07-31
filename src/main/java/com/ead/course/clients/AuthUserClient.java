73046b4df80d91148d8e6af621c0827ff7f39f67
Guilherme <guilhermesantos.srs@gmail.com>
2026-07-30 22:01:15 -0300

    Comunicação Síncrona - Inscrição de User em Course - Parte 2.

--- src/main/java/com/ead/course/clients/AuthUserClient.java
+++ src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -1,6 +1,5 @@
 package com.ead.course.clients;
 
-import com.ead.course.dtos.CourseDto;
 import com.ead.course.dtos.ResponsePageDto;
 import com.ead.course.dtos.UserDto;
 import com.ead.course.services.UtilsService;
@@ -21,7 +20,7 @@ import java.util.UUID;
 
 @Log4j2
 @Component
-public class CourseClient {
+public class AuthUserClient {
 
     @Autowired
     RestTemplate restTemplate;
@@ -49,5 +48,10 @@ public class CourseClient {
         }
         log.info("Ending request /users courseId {} ", courseId);
         return result.getBody();
+    }
+
+    public ResponseEntity<UserDto> getOneUserById(UUID userId) {
+        String url = REQUEST_URL_AUTHUSER + "/users/" + userId;
+        return restTemplate.exchange(url, HttpMethod.GET, null, UserDto.class);
     }
 }
