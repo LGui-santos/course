650ae8ab4933815ca5b9b975dfccb376bc9369ee
Guilherme <guilhermesantos.srs@gmail.com>
2026-07-30 21:07:41 -0300

    Comunicação Síncrona - Inscrição de User em Course - Parte 1.

--- src/main/java/com/ead/course/services/CourseUserService.java
+++ src/main/java/com/ead/course/services/CourseUserService.java
@@ -1,4 +1,13 @@
 package com.ead.course.services;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
 public interface CourseUserService {
+    boolean existsByCourseAndUserId(CourseModel courseModel, @NotNull UUID userId);
+
+    CourseUserModel save(CourseUserModel courseUserModel);
 }
